33235baea6ee753334df0bc0491a3182cf1e5a01
tarodo <rsvolozhanin@gmail.com>
2025-06-06 23:23:27 +0400

    fix: correct spotify scope

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     SPOTIFY_REDIRECT_URI: str = "http://127.0.0.1:8000/callback"
     SPOTIFY_AUTH_URL: str = "https://accounts.spotify.com/authorize"
     SPOTIFY_TOKEN_URL: str = "https://accounts.spotify.com/api/token"
-    SPOTIFY_SCOPE: str = "user-read-private user-read-email"
+    SPOTIFY_SCOPE: str = "user-read-private user-read-email user-read-playback-state user-modify-playback-state"
 
     class Config:
         env_file = ".env"
